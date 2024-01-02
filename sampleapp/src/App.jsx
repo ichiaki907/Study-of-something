35819bd1eede7c0c99abf3d6c8d05744447d67fd
ichiaki907 <ichiaki907@gmail.com>
2024-01-03 02:50:40 +0900

    Refactor App component and add ColorfulMessage component
    section6-48 end

---
+++ sampleapp/src/App.jsx
@@ -0,0 +1,16 @@
+import { ColorfulMessage } from "./components/ColorfulMessage";
+
+export const App = () => {
+  const onClickButton = () => alert();
+
+  return (
+    <>
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <ColorfulMessage color="blue">お元気ですか_</ColorfulMessage>
+      <ColorfulMessage color="green">元気です!</ColorfulMessage>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
---
+++ sampleapp/src/components/ColorfulMessage.jsx
@@ -0,0 +1,7 @@
+export const ColorfulMessage = ({ color, children }) => {
+    const contentStyleA = {
+    color,
+    fontSize: "18px",
+  };
+  return <p style={contentStyleA}>{children}</p>;
+};
