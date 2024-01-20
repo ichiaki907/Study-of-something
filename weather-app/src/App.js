d719b92aefd2f33cdcdccb3d2a88802d3ebf2299
ichiaki907 <ichiaki907@gmail.com>
2024-01-20 23:41:43 +0900

    Add create-react-app wether-app

---
+++ weather-app/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="max-w-lg mx-auto bg-black h-screen">
+      <h1 className="font-bold">Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
