4d1e2afe6ed6a38eb4dae349c012712978e0b5e5
ichiaki907 <ichiaki907@gmail.com>
2024-02-25 23:50:52 +0900

    Add: Sidebar component and dependencies

---
+++ react-sidebar/src/components/SidebarIcon.tsx
@@ -0,0 +1,12 @@
+import Icon from "../images/icons.jpg";
+
+function SidebarIcon() {
+  return (
+    <div className="SidebarIcon">
+      <img src={Icon} />
+      <p>test@gmail.com</p>
+    </div>
+  );
+}
+
+export default SidebarIcon;
