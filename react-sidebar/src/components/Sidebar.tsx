2f24410fa8cd0c1a48500d9470962901827f73d7
ichiaki907 <ichiaki907@gmail.com>
2024-02-26 00:00:59 +0900

    Add: react-sidebarのページ遷移をreact-routerに変更

--- react-sidebar/src/components/Sidebar.tsx
+++ react-sidebar/src/components/Sidebar.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
 import SidebarIcon from "./SidebarIcon";
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="Sidebar">
       <SidebarIcon />
@@ -13,7 +16,7 @@ const Sidebar = () => {
               id={window.location.pathname === value.link ? "active" : ""}
               className="row"
               onClick={() => {
-                window.location.pathname = value.link;
+                navigate(value.link);
               }}
             >
               <div id="icon">{value.icon}</div>
--- react-sidebar/src/main.tsx
+++ react-sidebar/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// root 要素を取得します。null チェックを行い、null でないことを保証します。
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
