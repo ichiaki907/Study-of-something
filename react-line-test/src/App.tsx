0d98877cf01d357beb68d085c35daa9c6d6f4c55
ichiaki907 <ichiaki907@gmail.com>
2024-02-29 23:54:18 +0900

    Add: @line/liffパッケージを追加し、LIFFの初期化と関連する関数を追加

--- react-line-test/src/App.tsx
+++ react-line-test/src/App.tsx
@@ -1,9 +1,31 @@
+import liff from "@line/liff";
 import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    liff
+      .init({
+        liffId: "2003854880-akRW4MEx", // Use own liffId
+      })
+      .then(() => {
+        console.log("LIFF init success");
+      });
+  }, []);
+
   return (
     <>
       <h1>Vite + React</h1>
+      <ul>
+        <li>liff.getLanguage(): {liff.getLanguage()}</li>
+        <li>liff.getVersion(): {liff.getVersion()}</li>
+        <li>
+          liff.isInClient():
+          {liff.isInClient() ? "LIFFブラウザ" : "外部ブラウザ"}
+        </li>
+        <li>liff.isLoggedIn(): {liff.isLoggedIn() ? "login" : "not login"}</li>
+        <li>liff.getOS(): {liff.getOS()}</li>
+      </ul>
     </>
   );
 }
