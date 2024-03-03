16c0369492895e6e9b53617d93c25fbea7659ab3
ichiaki907 <ichiaki907@gmail.com>
2024-03-03 23:46:11 +0900

    Add: tailwindcssとautoprefixerを追加、Appの出力を修正

--- react-line-test02/src/App.tsx
+++ react-line-test02/src/App.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Heading, Link, Text } from "@chakra-ui/react";
 
 function App() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [profile, setProfile] = useState({});
 
   useEffect(() => {
     liff
@@ -16,12 +17,15 @@ function App() {
       .then(() => {
         setMessage("LIFF init succeeded.");
         setIsLoggedIn(liff.isLoggedIn());
+        liff.getProfile().then((profile) => {
+          setProfile(profile);
+        });
       })
       .catch((e: Error) => {
         setMessage("LIFF init failed.");
         setError(`${e}`);
       });
-  });
+  }, []);
 
   const LineLoginhandler = () => {
     if (isLoggedIn === true) {
@@ -33,25 +37,30 @@ function App() {
   };
 
   return (
-    <Box className="App">
-      <h1>create-liff-app</h1>
-      {message && <p>{message}</p>}
+    <Box className="App mt-5">
+      <Heading>create-liff-app</Heading>
+      {message && <p className="mt-2">{message}</p>}
       {error && (
         <p>
           <code>{error}</code>
         </p>
       )}
-      <a
+      <Link
         href="https://developers.line.biz/ja/docs/liff/"
         target="_blank"
         rel="noreferrer"
       >
         LIFF Documentation
-      </a>
-      {isLoggedIn ? <p>ログイン中</p> : <p>ログアウト中</p>}
-      <button onClick={LineLoginhandler}>
+      </Link>
+      <Text fontSize="xl" className="mt-2 font-bold">
+        {isLoggedIn ? "ログイン中" : "ログアウト中"}
+      </Text>
+      <Button onClick={LineLoginhandler} colorScheme="teal" className="mt-2">
         {isLoggedIn ? "ログアウト" : "ログイン"}
-      </button>
+      </Button>
+      <Text fontSize="xl" className="mt-2">
+        {profile.displayName}
+      </Text>
     </Box>
   );
 }
